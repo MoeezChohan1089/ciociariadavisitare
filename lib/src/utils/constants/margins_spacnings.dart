19cedc017e062c80c40c46e270bea9192ce68161
Moeez Chohan <87126520+MoeezChohan1089@users.noreply.github.com>
2024-02-29 19:14:57 +0500

    Update margins_spacnings.dart

--- lib/src/utils/constants/margins_spacnings.dart
+++ lib/src/utils/constants/margins_spacnings.dart
@@ -1,5 +1,5 @@
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-final double pageMarginHorizontal = 16.w;
-final double pageMarginVertical = 16.h;
+final double pageMarginHorizontal = 16;
+final double pageMarginVertical = 16;
